790e83f4661e3dffbfa1d656bca2ddad099c9fbf
Erik Sundahl <esundahl@gmail.com>
2013-03-25 08:27:23 -0700

    updates

--- build/build.js
+++ build/build.js
@@ -266,15 +266,26 @@ Array.prototype.forEach.call(demos, function(demo) {
     Array.prototype.forEach.call(animated, function(anim) {
       var asdf = self.getAttribute('href');
       asdf = asdf.slice(1);
-      for (var i = anim.classList.length - 1; i >= 0; i--){
-        if (anim.classList[i] !== 'animated') {
-          anim.classList.remove(anim.classList[i]);
-        };
-      };
+      removeAnimationClasses(anim);
       anim.classList.add(asdf);
     });
   });
 });
 
+
+/**
+ * Removes all classes except for .animated
+ * @param  {el} el [dom element]
+ * @return {Void}
+ * @api private 
+ */
+
+function removeAnimationClasses (el) {
+  for (var i = el.classList.length - 1; i >= 0; i--){
+    if (el.classList[i] !== 'animated') {
+      el.classList.remove(el.classList[i]);
+    };
+  };
+}
 });
 
